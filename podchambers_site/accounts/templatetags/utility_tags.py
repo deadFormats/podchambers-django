67a725f67fab5ea3be7cfa903824214817034247
Dylan Garrett <dmgarrett72@protonmail.com>
2023-04-13 10:25:41 -0500

    trying to fix this front end but I might just worry about it later

--- podchambers_site/accounts/templatetags/utility_tags.py
+++ podchambers_site/accounts/templatetags/utility_tags.py
@@ -8,3 +8,12 @@ register = template.Library()
 @register.inclusion_tag('default.html', takes_context=True)
 def in_dev_template(**kwargs):
     return kwargs
+
+
+@register.filter
+def add_class_to_field(field, class_name):
+    if len(field.errors) > 0:
+        class_name += ' is-danger'
+    field_classes = field.field.widget.attrs.get('class', '')
+    field_classes += f' {class_name}'
+    return field.as_widget(attrs={'class': field_classes})
--- podchambers_site/podchambers_site/settings.py
+++ podchambers_site/podchambers_site/settings.py
@@ -134,5 +134,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'account.authentication.EmailAuthBackend',
+    'accounts.authentication.EmailAuthBackend',
 ]
